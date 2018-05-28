6721e1d40bddd22a39828cf042fd2a07e0d27faa
kadrikondi <abdulkadri42@gmail.com>
2018-05-28 18:15:33 +0100

    authentication/validation of login page

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+//const passport = require('passport');
 
-
+// load user model
+require ('../models/User');
+const User = mongoose.model('users');
 
 // user login route
 router.get('/login', (req,res)=>{
@@ -12,4 +16,60 @@ router.get('/login', (req,res)=>{
 router.get('/register', (req,res)=>{
     res.render('users/register');
 });
+// register form post
+router.post('/register', (req,res)=>{
+    //validation
+    let errors =[];
+
+    if (req.body.password !=req.body.password2){
+        errors.push({text:'passwords do not match'
+    });
+}
+if (req.body.password.length< 4){
+    errors.push({text:'password must be atleat 4 charaters'});
+}  
+if (errors.length >0){
+    res.render('users/register',{
+        errors:errors,
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        password2:req.body.password2
+    });
+//create a new user
+}else{
+    User.findOne({email: req.body.email})
+    .then(user=>{
+        if (user){
+            req.flash('error_msg', 'Email already Registered');
+            res.redirect('/users/register')
+        }else{
+
+            const newUser =  new User({
+                name :req.body.name,
+                email: req.body.email,
+                password:req.body.password
+            })
+            bcrypt.genSalt(10, (err,salt)=>{
+                bcrypt.hash(newUser.password, salt, (err,hash)=>{
+                    if (err) throw err;
+                    newUser.password =hash;
+                    newUser.save()
+                    .then(user =>{
+                     req.flash('success_msg' ,'You are now registered and can log in') ;
+                     res.redirect('/users/login') ;
+                    })
+                .catch(err=>{
+                    console.log(err);
+                    return;
+                })
+                });
+            })
+        
+
+        }
+    });
+    
+}
+})
 module.exports = router;
