9b827b04b8d8e6f6dc68a703c3abbdda0e66895f
kadrikondi <abdulkadri42@gmail.com>
2018-07-03 12:09:49 +0100

    add conditional statement

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,8 +2,9 @@ module.exports ={
     ensureAuthenticated: (req,res,next)=>{
         if(req.isAuthenticated()){
             return next();
-        }
+        }else{
         req.flash('error_msg','Not Authorized');
-        res.redirect('/users/login');
+        res.redirect('/login');
+    }
     }
 }
